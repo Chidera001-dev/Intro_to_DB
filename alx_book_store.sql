c75ea0245f9b196c19d9b0a1cf09fee151fd6a74
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-08-16 15:54:10 +0100

    added a alx_book_stores.sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,23 +1,17 @@
-import mysql.connector
+-- Create the database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Chidera@123#",
-    database="alx_book_store"
-)
-mycursor = mydb.cursor()
+-- Switch to the database
+USE alx_book_store;
 
 # Create Authors table
-mycursor.execute("""
 CREATE TABLE IF NOT EXISTS authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215)
 )
-""")
+
 
 # Create Books table (depends on authors)
-mycursor.execute("""
 CREATE TABLE IF NOT EXISTS books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130),
@@ -26,20 +20,18 @@ CREATE TABLE IF NOT EXISTS books (
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 )
-""")
+
 
 # create customers table
-mycursor.execute("""
 CREATE TABLE IF NOT EXISTS customers(
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 )
-""")
+
 
 # create orders table (depends on customers)
-mycursor.execute("""
 CREATE TABLE IF NOT EXISTS orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
@@ -47,10 +39,9 @@ CREATE TABLE IF NOT EXISTS orders(
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 
 )
-""")
+
 
 # create order_details table (depends on books and orders)
-mycursor.execute("""
 CREATE TABLE IF NOT EXISTS order_details(
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
@@ -60,6 +51,4 @@ CREATE TABLE IF NOT EXISTS order_details(
     FOREIGN KEY (book_id) REFERENCES books(book_id)
     
 )
-""" )
 
-print("✅ All tables created successfully!")
